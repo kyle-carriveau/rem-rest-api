ea150ee6900701c5737ae1c10fdb711b6c01819c
Kyle Carriveau <kylecarriveau@Kyles-MacBook-Air.local>
2024-12-06 11:46:42 -0700

    adding managers to the search filter for multiple property managers

--- leases/views.py
+++ leases/views.py
@@ -1,12 +1,13 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
+from authentication.permissions import IsPropertyManager
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Lease
 from .serializers import LeaseSerializer
 from .filters import LeaseFilter
 
 class LeaseListCreateView(ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsPropertyManager]
     serializer_class = LeaseSerializer
     filter_backends = [DjangoFilterBackend] # Add filter backend
     filterset_class = LeaseFilter # Add the filterset
@@ -15,18 +16,18 @@ class LeaseListCreateView(ListCreateAPIView):
         # Restrict to leases associated with properties managed by the current user
         # Optimize with select_related for foreign keys and prefetch_related for many-to-many relationships
         return (
-            Lease.objects.filter(property__created_by=self.request.user)
+            Lease.objects.filter(property__managers=self.request.user)
             .select_related("property", "tenant")
             .prefetch_related("property__tenants") # Optimize reverse relationship from Property to Tenants
         )
 
 class LeaseDetailView(RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsPropertyManager]
     serializer_class = LeaseSerializer
 
     def get_queryset(self):
         return (
-            Lease.objects.filter(property__created_by=self.request.user)
+            Lease.objects.filter(property__managers=self.request.user)
             .select_related("property", "tenant")
             .prefetch_related("property__tenants")
         )
